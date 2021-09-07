3b94fb8d7d30f76babacfa50cd6ab1219a9f3765
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-07 13:02:35 +0100

    took off new line

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     n = number % 10 if number >= 0 else (number * -1) % 10
-    print("{:d}".format(n))
+    print("{:d}".format(n), end="")
     return n
