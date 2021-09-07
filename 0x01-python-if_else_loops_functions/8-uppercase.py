c90f5bddda223d15939d0331eb84e8f9e730abd9
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-07 12:50:47 +0100

    fixed changed algorithm

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str[:-2]:
-        print("{}".format(chr(ord(char) - 32)), end="")
-    print("{}".format(chr(ord(str[-1]) - 32)))
-	
+    string = [0] * len(str)
+    for i in range(len(str)):
+        if ord(str[i]) > 96:
+            string[i] = chr(ord(str[i]) - 32)
+        else:
+            string[i] = str[i]
+        if i != len(str) - 1:
+            print("{}".format(string[i]),end="")
+    print("{}".format(string[i]))
