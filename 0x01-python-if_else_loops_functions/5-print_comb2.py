52c3a6ec277c4c6aa3749ed9e75c36b5a6d0ef03
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-07 11:46:03 +0100

    fix reversed numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print("{:0<2d}, ".format(i), end="")
+        print("{:0>2d}, ".format(i), end="")
     else:
-        print("{:0<2d}".format(i))
+        print("{:0>2d}".format(i))
