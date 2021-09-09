3f51216e64a9238b7598023109fc8865157240ba
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-09 09:54:41 +0100

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as cal
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, cal.add(a, b)))
+print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+print("{} / {} = {}".format(a, b, cal.div(a, b)))
