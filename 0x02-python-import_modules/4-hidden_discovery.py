41b6d01a4016102210ddb64f7e9e8c3c080e0a35
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-09 11:58:57 +0100

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import hidden_4
     for name in dir(hidden_4):
-        if not name.startswith('__'):
-            print('{:s}'.format(name))
+        if name.startswith('__'):
+            continue
+        print("{:s}".format(name))
