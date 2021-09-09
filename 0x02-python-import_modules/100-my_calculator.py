58e6f38ba611fd44546201cc3cf94cb84173522c
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-09 12:20:30 +0100

    task 6 done

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def main(argv):
+    argc = len(argv)
+    ops = {
+        '+' : operator.add,
+        '-' : operator.sub,
+        '*' : operator.mul,
+        '/' : operator.truediv,
+    }
+    if argc != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    res = ops[op](a, b)
+    print('{} {} {} = {}'.format(a, op, b, res))
+
+
+
+
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    import operator
+    main(argv)
