d084ad241432a07ba4452fbb497fb456f538fd31
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-09 10:13:44 +0100

    task 2 + indentation fix and run when not import only

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 1
+    if len(sys.argv) < 3:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for arg in sys.argv:
+        if arg == sys.argv[0]: continue
+        print("{}: {}".format(i, arg))
+        i += 1
