93476105a49f09f3629b510e29e5239ea9c7bd59
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-10 10:41:17 +0100

    return a copy instead of original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > len(my_list) - 1 or idx < 0:
-        return my_list
     new_list = my_list[:]
+    if idx > len(new_list) - 1 or idx < 0:
+        return new_list
     new_list[idx] = element
     return new_list
