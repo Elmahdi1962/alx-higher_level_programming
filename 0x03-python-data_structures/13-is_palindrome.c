c10cbeac05f731dc0f5f7fbe9604c8d199084cc8
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-13 11:02:30 +0100

    Fixed+ New algorithm

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,43 +9,61 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp_head = *head;
-	int list_len = 0, *list_numbers, i = 0;
+	listint_t *current = *head, *prev, *next, *left_head, *right_head;
+	int list_len = 0, i = 0, not_p = 0;
 
 	if (*head == NULL || head == NULL)
 		return (1);
 	/* count how many nodes the list have */
-	while (tmp_head != NULL)
+	while (current != NULL)
 	{
 		list_len++;
-		tmp_head = tmp_head->next;
+		current = current->next;
 	}
 	/* if the list has one node it's counted as palindrome */
 	if (list_len == 1)
 		return (1);
-	/* create an array to hold the numbers in each node */
-	list_numbers = malloc(sizeof(int) * list_len);
-	if (list_numbers == NULL)
-		return (0);
-	tmp_head = *head;
-	/* loop thru the list again and assign each*/
-	/* node's number to the array we created */
-	while (tmp_head != NULL)
+	/* reverse left half */
+	current = *head;
+	for (i = 1; i <= list_len / 2 && current != NULL; i++)
 	{
-		list_numbers[i] = tmp_head->n;
-		tmp_head = tmp_head->next;
-		i++;
-	}
-	/* loop thru the array of numbers to half + 1 */
-	/* and compare it to it's opposite index */
-	for (i = 0; i < (list_len + 1) / 2; i++)
-	{
-		if (list_numbers[i] != list_numbers[(list_len - 1) - i])
+		next = current->next;
+		if (prev != NULL)
+			current->next = prev;
+		else
 		{
-			free(list_numbers);
-			return (0);
+			current->next = NULL;
 		}
+		prev = current;
+		current = next;
 	}
-	free(list_numbers);
+	/* prev == to head of reversed left half and current == head of right half */
+	right_head = current;
+	left_head = prev;
+	for (i = 1; i <= list_len / 2 && current != NULL; i++)
+	{
+		if (list_len % 2 != 0 && i == 1)
+			current = current->next;
+		if (current->n != prev->n)
+		{
+			not_p = 1;
+			break;
+		}
+		current = current->next;
+		prev = prev->next;
+	}
+	/* reverse the left half back like it was before */
+	current = left_head;
+	prev = right_head;
+	for (i = 1; i <= list_len / 2 && current != NULL; i++)
+	{
+		next = current->next;
+		if (prev != NULL)
+			current->next = prev;
+		prev = current;
+		current = next;
+	}
+	if (not_p == 1)
+		return (0);
 	return (1);
 }
