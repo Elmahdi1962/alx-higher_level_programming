d25862bfa7a1cd88098f2c22e70e2fcf32e338af
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-13 11:07:46 +0100

    Fixed betty style

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,16 +14,10 @@ int is_palindrome(listint_t **head)
 
 	if (*head == NULL || head == NULL)
 		return (1);
-	/* count how many nodes the list have */
 	while (current != NULL)
-	{
-		list_len++;
-		current = current->next;
-	}
-	/* if the list has one node it's counted as palindrome */
+		list_len++, current = current->next;
 	if (list_len == 1)
 		return (1);
-	/* reverse left half */
 	current = *head;
 	for (i = 1; i <= list_len / 2 && current != NULL; i++)
 	{
@@ -31,15 +25,10 @@ int is_palindrome(listint_t **head)
 		if (prev != NULL)
 			current->next = prev;
 		else
-		{
 			current->next = NULL;
-		}
-		prev = current;
-		current = next;
+		prev = current, current = next;
 	}
-	/* prev == to head of reversed left half and current == head of right half */
-	right_head = current;
-	left_head = prev;
+	right_head = current, left_head = prev;
 	for (i = 1; i <= list_len / 2 && current != NULL; i++)
 	{
 		if (list_len % 2 != 0 && i == 1)
@@ -49,21 +38,15 @@ int is_palindrome(listint_t **head)
 			not_p = 1;
 			break;
 		}
-		current = current->next;
-		prev = prev->next;
+		current = current->next, prev = prev->next;
 	}
-	/* reverse the left half back like it was before */
-	current = left_head;
-	prev = right_head;
+	current = left_head, prev = right_head;
 	for (i = 1; i <= list_len / 2 && current != NULL; i++)
 	{
 		next = current->next;
 		if (prev != NULL)
 			current->next = prev;
-		prev = current;
-		current = next;
+		prev = current, current = next;
 	}
-	if (not_p == 1)
-		return (0);
-	return (1);
+	return (not_p == 1 ? 0 : 1);
 }
