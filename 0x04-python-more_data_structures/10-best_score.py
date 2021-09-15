caad34d3b430213d4ebdc177bcd528af944bd209
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-15 12:47:02 +0100

    task 10 done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/sur/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    bgs = -1
+    bgk = ''
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > bgs:
+            bgs = a_dictionary[key]
+            bgk = key
+    return bgk
