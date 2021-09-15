2ded8b56099a66fa5c44ab4c7b3fe911ba17faba
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-15 14:06:33 +0100

    Fixed (converted nested map object to list)

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda l: map(lambda n: n * n, l), matrix))
+    return list(map(lambda l: list(map(lambda n: n * n, l)), matrix))
