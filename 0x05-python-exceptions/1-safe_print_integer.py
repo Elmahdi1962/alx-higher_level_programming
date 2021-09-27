a91a012c62d57077b396ef99af0f5b538ef57325
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-27 10:35:51 +0100

    used a variable instead of returning true or false directly

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    is_int = True
     try:
         print("{:d}".format(value))
     except ValueError:
-        return False
-    return True
+        is_int = False
+    return is_int
