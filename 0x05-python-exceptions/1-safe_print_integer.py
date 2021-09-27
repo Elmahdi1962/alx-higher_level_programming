2ba4dcd0b021b95ccb808d2e2095de69b2db5402
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-27 10:41:42 +0100

    raising exception when value is boolean too manually

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     is_int = True
     try:
+        if value == True or value == False:
+            raise ValueError
         print("{:d}".format(value))
     except ValueError:
         is_int = False
