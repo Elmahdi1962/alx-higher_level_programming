17295b95341be7b191c29bc620448287034cd5f7
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-28 12:55:04 +0100

    fixed pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-""" this module defines a class Square"""
+"""this module defines a class Square."""
 
 
 from typing import Type
 
 
 class Square:
-    """ class for Square"""
+    """class for Square."""
+
     def __init__(self, size=0):
-        """ Initialize class and check if it's an integer and if less than 0
+        """Initialize class and check if it's an integer and if less than 0.
+
         args:
-            size (int, optional): size of the square
+            size (int, optional): size of the square.
         Raises:
-            TypeError: raised when the arg size is not integer
-            ValueError: raised when the arg size is lees than 0
+            TypeError: raised when the arg size is not integer.
+            ValueError: raised when the arg size is lees than 0.
         """
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
