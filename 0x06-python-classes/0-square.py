25e7bb89b10143a0097da642a237a21d7cdba3bb
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-28 12:32:44 +0100

    fixed pycodestyle errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" this module defines a empty class square"""
+""" this module defines a empty class square """
+
 
 class Square:
-    """ empty class for task 0 
-    """
+    """ empty class for task 0 """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ this module defines a class Square"""
 
+
 class Square:
     """ class for Square"""
     def  __init__(self, size):
