8f3399ca0072aa112cd0af3fe97108412730991c
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-28 14:31:36 +0100

    fixed pycode style error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,7 +68,11 @@ class Square:
         Returns:
             None
         """
-        if type(value) != tuple or type(value[0]) != int or type(value[1]) != int or value[0] < 0 or value[1] < 0 or len(value) != 2:
+        if type(value) != tuple or type(value[0]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) != int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[1] < 0 or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
