ea82347218af46b19a7e213f1f54f5d164e39ba2
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-28 12:57:33 +0100

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""this module defines a class Square."""
-
-
-from typing import Type
+""" creates class Square """
 
 
 class Square:
-    """class for Square."""
-
+    """ Square class"""
     def __init__(self, size=0):
-        """Initialize class and check if it's an integer and if less than 0."""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
