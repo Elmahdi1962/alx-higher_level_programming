d5faeba164c40b9643b7c2ce86831ceb0da9e0ab
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-02 17:52:32 +0100

    task 7 done

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''this module for matrices calculations using numpy'''
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    '''multipy 2 patrices with numpy'''
+    return np.dot(m_a, m_b)
