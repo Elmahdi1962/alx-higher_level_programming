8cfdd21d724da5997b01c42fb248f701e0866066
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-03 15:38:19 +0100

    task 8  done

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,31 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_string : prints info about python string
+ * @p: pyobject
+ * Return: void
+ */
+
+void print_python_string(PyObject *p)
+{
+	long int length;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
