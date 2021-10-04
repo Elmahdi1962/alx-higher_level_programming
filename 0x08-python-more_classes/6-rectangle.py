c055c02d1a67543a28e0dd2daa0e92906a06612a
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-04 12:40:07 +0100

    task 6 done

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+'''module for shapes'''
+
+
+class Rectangle:
+    '''class for rectangle'''
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        '''Initiation of object'''
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        '''gets the width attr'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''sets the width attr'''
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        '''gets the height attr'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''sets the height attr'''
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        '''returns the area of the rectangle'''
+        return self.__height * self.width
+
+    def perimeter(self):
+        '''return the perimeter of the rectangle'''
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height * 2) + (self.__width * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        else:
+            hashes = '#' * self.__width
+            return '\n'.join(hashes for h in range(self.__height))
+
+    def __repr__(self):
+        return("Rectangle({}, {})".format(self.width, self.height))
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print('Bye rectangle...')
