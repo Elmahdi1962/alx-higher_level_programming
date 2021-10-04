ce9e9b4d18331294c0ffaa6e0742c58da4b898dd
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-04 11:38:03 +0100

    task 1 done

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+'''module for shapes'''
+
+
+class Rectangle:
+    '''class for rectangle'''
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+    @property
+    def height(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
