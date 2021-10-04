d2e457bd8be44bef7251ff01cd6e2bcd5c690e7c
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-04 13:08:45 +0100

    fixed naming error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -32,4 +32,4 @@ class Rectangle:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
-        self.height = value
+        self.__height = value
