61ac10e2e21b24bebfdacf00d8017a48e4883f5a
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-05 14:35:17 +0100

    fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+'''class that rejcts __dict__ and uses slots instead'''
 
 
 class LockedClass:
+    '''this is the locked class'''
 
     __slots__ = ["first_name"]
-
-    def __init__(self):
-        pass
