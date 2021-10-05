72c2a28bac8b0cd8b1c04322de7df303a8724d9f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-05 14:30:41 +0100

    fixed pycodestyle errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
+
 class LockedClass:
+
     def __setattr__(self, key, value):
         if key == 'first_name':
             object.__setattr__(self, key, value)
         else:
-            raise AttributeError("'LockedClass' object has no attribute '{:s}'".format(key))
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{:s}'".format(key)
+                )
 
     def __init__(self):
         pass
