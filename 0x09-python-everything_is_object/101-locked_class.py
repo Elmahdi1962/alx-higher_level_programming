7ea230878dc26735fa68a717b7927878fefbfd85
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-05 14:28:35 +0100

    task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    def __setattr__(self, key, value):
+        if key == 'first_name':
+            object.__setattr__(self, key, value)
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '{:s}'".format(key))
+
+    def __init__(self):
+        pass
