0f4b88522f2fbadfc4f5341044f773d9d804ee9d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-05 14:18:14 +0100

    code change

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return (', '.join(l))
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
