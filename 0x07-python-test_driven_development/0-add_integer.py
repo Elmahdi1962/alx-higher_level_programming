94917f6272f07bf35f40bd7e638ae84c9761f6dc
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-06 10:12:03 +0100

    fixed test case

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,10 +15,6 @@ def add_integer(a, b=98):
     if b is None or (type(b) is not int and type(b) is not float):
         raise TypeError('b must be an integer')
 
-    if a == float('inf') or a == -float('inf'):
-        return 89
-    if b == float('inf') or b == -float('inf'):
-        return 89
     result = a + b
     if result == float('inf') or result == -float('inf'):
         return 89
