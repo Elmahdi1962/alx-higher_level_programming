5cabced48dc0259cc91138128cd88d618c9ec705
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-06 13:37:12 +0100

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,28 +3,9 @@
 
 
 def text_indentation(text):
-    '''this function fixes text indentation
-    args:
-        text (str): text to fix
-    returns:
-        None
-    '''
-
-    if type(text) is not str:
-        raise TypeError('text must be a string')
-
-    fst_idx = 0
-    for idx, char in enumerate(text):
-        if char == '.' or char == '?' or char == ':':
-            if (idx + 1) == len(text):
-                tmp_str = text[fst_idx:]
-            else:
-                tmp_str = text[fst_idx:idx + 1]
-            fst_idx = idx + 1
-            print(tmp_str.strip())
-            print()
-            continue
-
-        if idx == len(text) - 1:
-            tmp_str = text[fst_idx:]
-            print(tmp_str.strip())
+    """Format text by splitting lines at special characters"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for char in '.:?':
+        text = text.replace(char, char + '\n\n')
+    print(*(ln.strip() for ln in (text + '\n').splitlines()), sep='\n', end='')
