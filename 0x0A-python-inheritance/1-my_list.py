dc4b9844ba12b73e3f98331806b0d139690f147c
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 13:16:37 +0100

    took off the inin

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """class utilizing list class"""
 
-    def __init__(self):
-        """initializer for MyList"""
-        pass
-
     def print_sorted(self):
         """print sorted list"""
         res = list.copy(self)
