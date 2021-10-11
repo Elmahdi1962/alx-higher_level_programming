3bb3215db9dd69c3bac5c6ad344d4273f9c3ceea
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 12:54:14 +0100

    fixed comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,11 @@
 
 
 class MyList(list):
+    '''class that derives from list class'''
+
     def print_sorted(self):
+        '''function that prints the list sorted way ascending'''
+
         newl = []
         for e in sorted(self):
             newl.append(e)
