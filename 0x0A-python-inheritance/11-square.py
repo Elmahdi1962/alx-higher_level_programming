0b2c1ebdb148e7bc2a0f77ad5296612174962af4
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 15:06:05 +0100

    task 11 done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''task 9 module'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''classy class'''
+    def __init__(self, size):
+        '''initialization of squar'''
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        '''get that number'''
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__width}/{self.__height}"
