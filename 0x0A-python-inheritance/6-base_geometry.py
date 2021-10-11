39518d1a39daa1528b60c0aeee57f996e98caa52
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 13:50:24 +0100

    task 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''task 6 module'''
+
+
+class BaseGeometry:
+    '''empty class'''
+    def __init__(self):
+        '''empty init'''
+        pass
+
+    def area(self):
+        '''stupid function'''
+        raise Exception('area() is not implemented')
