103da9fce9bd749fca833312c49cf6d068c9103f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 14:07:41 +0100

    task 7 done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''task 7 module'''
+
+
+from typing import Type
+
+
+class BaseGeometry:
+    '''empty class'''
+    def __init__(self):
+        '''empty init'''
+        pass
+
+    def area(self):
+        '''stupid function'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''another useless function'''
+        if type(value) is not int:
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
