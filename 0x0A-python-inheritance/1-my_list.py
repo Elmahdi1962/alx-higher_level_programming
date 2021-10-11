d5371c659087f0429b7d57675ae987407b808602
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 13:04:52 +0100

    fixed method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,14 +5,8 @@
 class MyList(list):
     '''class that derives from list class'''
 
-    def __init__(self):
-        '''Initialization of the subclass'''
-        pass
-
     def print_sorted(self):
         '''function that prints the list sorted way ascending'''
-
-        newl = []
-        for e in sorted(self):
-            newl.append(e)
-        print(newl)
+        l = list.copy(self)
+        list.sort(l)
+        print(l)
