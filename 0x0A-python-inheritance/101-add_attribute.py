146b3fe8d912a29fcb0e4f29c170810d93d16227
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-11 15:38:13 +0100

    Fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(object, name, value):
     '''this function adds new attr to the obj if possible'''
-    if '__dict__' in dir(object):
+    if '__dict__' in dir(object) and type(object.__dict__) is dict:
         object.name = value
     else:
         raise TypeError("can't add new attribute")
