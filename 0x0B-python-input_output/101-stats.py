92ff3873697f72ff14e5271b0f02aa37392fc64e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-12 17:09:18 +0100

    Fixed and Done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -24,16 +24,17 @@ def printer():
     print(f'File size: {total_size}')
     for key, value in sorted(status_codes.items()):
         if value > 0:
-            print('{:s}: {:d}'.format(key, value))
+            print('{:d}: {:d}'.format(key, value))
 
 
 try:
     for line in stdin:
         splitted_line = line.split()
-        status = splitted_line[-2]
-        total_size += int(splitted_line[-1])
-        if status in status_codes:
-            status_codes[status] += 1
+        if len(splitted_line) >= 2:
+            status = splitted_line[-2]
+            total_size += int(splitted_line[-1])
+            if status in status_codes:
+                status_codes[status] += 1
         i += 1
 
         if i % 10 == 0:
