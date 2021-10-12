c0f0e6ac9ea02ca8c861830b292c3b7e6020960f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-12 17:01:47 +0100

    test3

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,14 +6,14 @@ from sys import stdin
 
 
 status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
         }
 
 total_size = i = 0
@@ -24,14 +24,14 @@ def printer():
     print(f'File size: {total_size}')
     for key, value in sorted(status_codes.items()):
         if value > 0:
-            print('{:d}: {:d}'.format(key, value))
+            print('{:s}: {:d}'.format(key, value))
 
 
 try:
     for line in stdin:
         splitted_line = line.split()
         if len(splitted_line) >= 2:
-            status = int(splitted_line[-2])
+            status = splitted_line[-2]
             total_size += int(splitted_line[-1])
             if status in status_codes:
                 status_codes[status] += 1
