1664fe23f865ddd38d13b4e419d6a41d97420c1d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-12 14:34:45 +0100

    fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,27 +3,30 @@
 
 
 def pascal_triangle(n):
-    '''returns a list of lists of integers representing the Pascal’s triangle of n'''
+    '''returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    '''
     if n <= 0:
         return list()
 
     tr = []
-    for line in range(0, n) :
-         
+    for line in range(0, n):
         # Every line has number of
         # integers equal to line
         # number
         tmp = []
-        for i in range(0, line + 1) :
+        for i in range(0, line + 1):
             tmp.append(magic(line, i))
         tr.append(tmp)
     return tr
-     
-def magic(n, k) :
+
+
+def magic(n, k):
+    '''magic function that does some magic'''
     res = 1
-    if (k > n - k) :
+    if (k > n - k):
         k = n - k
-    for i in range(0 , k) :
+    for i in range(0, k):
         res = res * (n - i)
         res = res // (i + 1)
     return res
