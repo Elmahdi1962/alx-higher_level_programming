0b4120bad1474481bc4873e2512279c10e5d3ead
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-12 12:41:46 +0100

    test using dump

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''writes obj to file in json format'''
     with open(filename, mode='w', encoding='utf-8') as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
