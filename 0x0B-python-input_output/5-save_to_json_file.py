65b36f7fc15aa0b68bb40e910de53b74417bb435
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-12 12:40:31 +0100

    task 5 done

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''task 5 module'''
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''writes obj to file in json format'''
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dumps(my_obj, f)
