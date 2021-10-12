92c2287b7bc300dd23d5c4dda81d11f3ae4bbbaf
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-12 12:05:53 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''module for task 0'''
+
+
+def read_file(filename=""):
+    '''reads a file in utf-8 encoding'''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
