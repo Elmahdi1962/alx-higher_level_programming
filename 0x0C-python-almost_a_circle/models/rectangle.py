4c31a518cc022d8365d06066c2122dc3501fd5f5
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-14 12:19:25 +0100

    task 3 , update class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''sets the new value to the __width attribute'''
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     #  height Property
@@ -33,6 +37,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         '''sets the new value to the __height attribute'''
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     #  x Property
@@ -44,6 +52,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         '''sets the new value to the __x attribute'''
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     #  y Property
@@ -55,4 +67,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         '''sets the new value to the __y attribute'''
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
