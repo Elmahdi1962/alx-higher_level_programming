97230ac8c6fc45b7ee351b40ac6168720d5116b1
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-18 18:36:03 +0100

    update to_json_string method in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,7 +31,8 @@ class Base:
         '''returns the Json string reprisentaion
         of the argument list_dictionaries
         '''
-        if list_dictionaries is None or len(list_dictionaries) <= 0:
+        if (list_dictionaries is None or len(list_dictionaries) <= 0 or
+                list_dictionaries == []):
             return "[]"
         return json.dumps(list_dictionaries)
 
