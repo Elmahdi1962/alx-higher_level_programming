5df74305552ee251c5e5e7e12bb0bbd3dd11a58f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-18 21:08:14 +0100

    fixed to_json_string method in base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,9 +32,9 @@ class Base:
         of the argument list_dictionaries
         '''
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return "[]"
         if len(list_dictionaries) <= 0:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
