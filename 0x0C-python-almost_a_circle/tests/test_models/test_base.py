fbc15c04cbf3884c115e922512d00c0f712e48e0
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-19 09:42:41 +0100

    test3

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -27,6 +27,7 @@ class TestBase(unittest.TestCase):
         '''creating an instance of the class Base with out passing the id'''
         obj = Base()
         obj2 = Base()
+        self.assertEqual(Base().id, 7)
         self.assertEqual(obj.id, 5)
         self.assertEqual(obj2.id, 6)
 
