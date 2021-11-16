cf936c3959832e5616c4f123a7c9adfe3526cc30
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-16 16:29:23 +0000

    task 18

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+USE mysql;
+USE hbtn_0c_0;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
