00a6f6754e30f914d7d93177343b9357e3510932
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-16 16:14:02 +0000

    fix task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,8 @@
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+-- use the database
+USE hbtn_0c_0;
 -- convert first_table to utf-8
 ALTER TABLE first_table
 CHARACTER SET utf8mb4
