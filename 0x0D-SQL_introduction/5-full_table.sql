995e70fe97933baf19eedbedb483d789f6a0022b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-16 15:12:39 +0000

    fied task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- print descriptio of the table first_table
-SHOW first_table;
+SHOW CREATE TABLE first_table;
