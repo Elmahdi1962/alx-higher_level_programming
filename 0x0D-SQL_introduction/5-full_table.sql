4c44cac4193371bf002ab21d6eeac48c648c3f9e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-16 14:50:36 +0000

    added the forgoten semicolon

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- print descriptio of the table first_table
-DESC first_table
+DESC first_table;
