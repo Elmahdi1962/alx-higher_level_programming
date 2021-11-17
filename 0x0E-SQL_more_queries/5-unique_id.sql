fc4b72b529ad5bf45889ceb6866b5a7f9ee06b0d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-17 12:40:49 +0100

    UPDATE

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 -- creates the table unique_id on your MySQL server.
 CREATE TABEL IF NOT EXISTS unique_id(
-	id INT DEFAULT 1,
-	name VARCHAR(256),
-	CONSTRAINT uniq_id UNIQUE (id)
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256)
 );
