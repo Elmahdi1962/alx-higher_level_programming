397b9143b65c25625735fe085764ff9ce5ba0f8f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-17 12:13:18 +0100

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2.
+-- create database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grant select privilege
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
