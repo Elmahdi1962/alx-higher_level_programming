57bdd8a7baa38fff941a4cf1ecda3c26c49434e5
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-13 16:28:38 +0100

    update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () { myObject.value++; };
+myObject.incr = function () { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
