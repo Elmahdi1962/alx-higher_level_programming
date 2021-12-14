61bc19b27a8ced3e19369c19d40b617815f98857
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-14 15:29:01 +0100

    task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+
+let text = '';
+
+text += fs.readFileSync(fileA, (error) => {
+  if (error) throw error;
+});
+
+text += '\n';
+
+text += fs.readFileSync(fileB, (error) => {
+  if (error) throw error;
+});
+
+fs.writeFile(fileC, text, (error) => {
+  if (error) throw error;
+});
