b937745b461c97433125e4f499860a0fe5a41fa6
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-14 15:40:33 +0100

    task 13 update

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,18 +5,22 @@ const fileC = process.argv[4];
 const fs = require('fs');
 
 let text = '';
-if (fileA && fileB && fileC) {
-  text += fs.readFileSync(fileA, (error) => {
-    if (error) throw error;
-  });
+if (fileA !== undefined && fileB !== undefined &&
+    fileC !== undefined) {
+  if ((fs.existsSync(fileA) && fs.statSync(fileA).isFile) &&
+    (fs.existsSync(fileB) && fs.statSync(fileB).isFile)) {
+    text += fs.readFileSync(fileA, (error) => {
+      if (error) throw error;
+    });
 
-  text += '\n';
+    text += '\n';
 
-  text += fs.readFileSync(fileB, (error) => {
-    if (error) throw error;
-  });
+    text += fs.readFileSync(fileB, (error) => {
+      if (error) throw error;
+    });
 
-  fs.writeFile(fileC, text, (error) => {
-    if (error) throw error;
-  });
+    fs.writeFile(fileC, text, (error) => {
+      if (error) throw error;
+    });
+  }
 }
