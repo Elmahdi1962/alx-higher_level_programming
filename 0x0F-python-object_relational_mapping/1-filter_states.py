45a7198cc124934e9424b86c55b5a47e1e33ec92
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-16 14:34:51 +0100

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''script for task 1'''
+
+import MySQLdb
+import sys
+
+
+def list_N():
+    '''lists all states with a name that starts with N'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name regexp "^N."; ORDERBY id ASC')
+    result = cur.fetchall()
+
+    if result:
+        for row in result:
+            print(row)
+
+
+if __name__ == "__main__":
+    list_N()
