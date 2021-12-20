5866481bca821f06029face83226363ab5389687
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-12-20 10:38:17 +0100

    update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 '''script for task 16'''
 
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+import sys
 
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
 
-if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                            username, password, host, port, db_name
+                            ), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    loc_session = Session()
+    states = loc_session.query(State).join(City).order_by(
+        State.id, City.id
+        ).all()
 
-    session = sessionmaker(bind=engine)()
-    states = session.query(State).order_by(State.id).all()
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-    session.commit()
-    session.close()
+            print('\t{}: {}'.format(city.id, city.name))
+
+    loc_session.close()
+    engine.dispose()
