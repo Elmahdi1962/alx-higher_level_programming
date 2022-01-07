f9b9b7ac9ddcbb9342497cc282310bff1732bb38
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-07 14:01:51 +0100

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''task 6 module'''
+
+def find_peak(list_of_integers):
+    '''finds a peak in a list of unsorted integers'''
+    last_peak = None
+    for idx, n in enumerate(list_of_integers):
+        if idx > 0 and idx < len(list_of_integers) - 1:
+            if n > list_of_integers[idx + 1]:
+                if n > list_of_integers[idx - 1]:
+                    return n
+            elif n == list_of_integers[idx + 1]:
+                if n == list_of_integers[idx - 1]:
+                    if last_peak is not None and last_peak < n:
+                        last_peak = n
+                    elif last_peak is None:
+                        last_peak = n
+    if last_peak is not None:
+        return last_peak
+    return None
