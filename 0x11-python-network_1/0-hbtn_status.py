5b5e728e4988b0c1227b23010d54e0932a09e54e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-08 11:05:31 +0100

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,9 @@
 if __name__ == '__main__':
     import urllib.request
 
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-            content = res.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except Exception:
-        pass
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
