6e164ce7231d07b18f6480f957d0624e77264666
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-08 10:53:21 +0100

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 # script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sL -H "Content-type:application/json"  -d @"$2" -X POST "$1"
+curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
