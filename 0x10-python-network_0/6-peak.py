82ebceddcfee4d96581769a6a03ae7b37c3af579
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-10 09:40:11 +0100

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-'''task 6 module'''
-
-
-def find_peak(list_of_integers):
-    '''finds a peak in a list of unsorted integers'''
-    last_peak = None
-    for idx, n in enumerate(list_of_integers):
-        if idx > 0 and idx < len(list_of_integers) - 1:
-            if n > list_of_integers[idx + 1]:
-                if n > list_of_integers[idx - 1]:
-                    return n
-            elif n == list_of_integers[idx + 1]:
-                if n == list_of_integers[idx - 1]:
-                    if last_peak is not None and last_peak < n:
-                        last_peak = n
-                    elif last_peak is None:
-                        last_peak = n
-    if last_peak is not None:
-        return last_peak
-    return None
+# O(log(n))
+def find_peak3(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if not isinstance(list_of_integers, list) or len(list_of_integers) == 0:
+        return None
+    n = len(list_of_integers)
+    mid = n // 2
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[0:mid - 1])
+    elif mid + 1 < n and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:n])
+    elif mid > 0 and list_of_integers[mid] == list_of_integers[mid - 1]:
+        if n % 2 == 0:
+            return find_peak(list_of_integers[0:mid])
+        else:
+            return find_peak(list_of_integers[mid:n])
+    else:
+        return list_of_integers[mid]
