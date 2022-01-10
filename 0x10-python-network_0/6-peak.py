21e9f716c840824c69278a3916baa898db8ff8b2
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-10 09:43:52 +0100

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-# O(log(n))
-def find_peak3(list_of_integers):
+"""task 6 script"""
+
+
+def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
-    if not isinstance(list_of_integers, list) or len(list_of_integers) == 0:
-        return None
-    n = len(list_of_integers)
-    mid = n // 2
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[0:mid - 1])
-    elif mid + 1 < n and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:n])
-    elif mid > 0 and list_of_integers[mid] == list_of_integers[mid - 1]:
-        if n % 2 == 0:
-            return find_peak(list_of_integers[0:mid])
-        else:
-            return find_peak(list_of_integers[mid:n])
-    else:
-        return list_of_integers[mid]
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
