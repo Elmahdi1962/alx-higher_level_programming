e9d6f801870688999ffe447aa7a8ca01fdd33284
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 11:35:44 +0100

    removed unused module

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const { json } = require('stream/consumers');
 
 if (process.argv.length > 2) {
   const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
