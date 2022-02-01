e709d8ff7bdceb828323b511782c68f026d2f731
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 10:37:22 +0100

    chnaged console.error with console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
