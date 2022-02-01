c7be974823c1bf072e5276f5ec293a76042157ce
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 12:18:34 +0100

    dfthf

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,14 @@
 const request = require('request');
 
 if (process.argv.length > 2) {
-  const charLink = 'https://swapi-api.hbtn.io/api/people/18/';
   request(process.argv[2], (err, res, body) => {
     if (err) {
       console.log(err);
-    } else if (body){
-      const movies = JSON.parse(body);
-      const result = movies.results.filter(item => item.characters.includes(charLink));
-      console.log(result.length);
+    } else {
+      const charFilms = JSON.parse(body).results.filter(
+        x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+      );
+      console.log(charFilms.length);
     }
   });
 }
