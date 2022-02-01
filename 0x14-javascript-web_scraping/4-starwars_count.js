34c70b26316749ca3e7bd7f11fcddd657f626dee
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 12:29:41 +0100

    fix : changed includes with find and match

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,11 @@
 const request = require('request');
 
 if (process.argv.length > 2) {
-  const charLink = 'https://swapi-api.hbtn.io/api/people/18/';
   request(process.argv[2], (err, res, body) => {
     if (err) {
       console.log(err);
     } else {
-      const result = JSON.parse(body).results.filter(item => item.characters.includes(charLink));
+      const result = JSON.parse(body).results.filter(item => item.characters.find(id => id.match(/18/)));
       console.log(result.length);
     }
   });
