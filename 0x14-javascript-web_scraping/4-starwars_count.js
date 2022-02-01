dcf595aa9d7a8e80efc2b63d1a9278295890c0ef
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 12:01:09 +0100

    update : use filter instead of foreach

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,10 @@ const request = require('request');
 
 if (process.argv.length > 2) {
   const charLink = 'https://swapi-api.hbtn.io/api/people/18/';
-  let count = 0;
   request(process.argv[2], (err, res, body) => {
     if (err) console.log(err);
     const movies = JSON.parse(body);
-    Array.prototype.forEach.call(movies.results, (item, idx) => {
-      if (item.characters.includes(charLink)) {
-        count++;
-      }
-    });
-    console.log(count);
+    const result = movies.results.filter(item => item.characters.includes(charLink));
+    console.log(result.length);
   });
 }
