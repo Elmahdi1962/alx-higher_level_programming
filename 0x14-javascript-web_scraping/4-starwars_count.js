4b1fd167d1acd44ab1d4746ff4518259c6226f1c
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 12:12:56 +0100

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,10 @@ if (process.argv.length > 2) {
   const charLink = 'https://swapi-api.hbtn.io/api/people/18/';
   request(process.argv[2], (err, res, body) => {
     if (err) console.log(err);
-    const movies = JSON.parse(body);
-    const result = movies.results.filter(item => item.characters.includes(charLink));
-    console.log(result.length);
+    else {
+      const movies = JSON.parse(body);
+      const result = movies.results.filter(item => item.characters.includes(charLink));
+      console.log(result.length);
+    }
   });
 }
