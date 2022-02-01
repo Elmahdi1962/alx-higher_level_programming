4c86d0abfa4f7730f1a25f8719ba0139ae280411
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 10:51:11 +0100

    update fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data.toString('utf-8'));
-});
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
+}
