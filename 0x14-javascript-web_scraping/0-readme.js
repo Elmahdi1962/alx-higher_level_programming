ea555f775ded2a6f6f0a8e65ccff8af33274f503
Elmahdi <74487643+Elmahdi1962@users.noreply.github.com>
2022-02-01 10:55:03 +0100

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 if (process.argv.length > 2) {
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  fs.readFile(process.argv[2], (err, data) => {
     if (err) {
       console.log(err);
     } else {
