e15f3dae53131bab96ee109c34e5c4e76f9fa36e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-02-09 12:09:11 +0100

    task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,5 @@
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
-  $('div#character').text(data.name);
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data, status) {
+  if (status === 'success') {
+    $('div#character').text(data.name);
+  }
 });
